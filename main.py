7491a80709c4cb1c40bd3d6d92bef9b934e3b908
Hermes Thoth <52197701+MihleDex@users.noreply.github.com>
2022-05-20 11:01:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import title
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'fcc8cd976388abead7c7a207762cb7c5'
+
 @app.route('/')
-@app.route('/home')
 def index():
     return render_template('home.html',title="Home")
 
@@ -30,6 +32,26 @@ def men():
 @app.route('/sales')
 def sales():
     return render_template('sales.html',title="Sales")
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('index'))
+    return render_template('register.html',title="Register",form=form)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print("hey")
+        if form.email.data == 'admin@site.com' and form.password.data == 'password':
+            flash('Login Success!','success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful!','danger')
+    return render_template('login.html',title="Login",form=form)
 
 if __name__ == '__main__':
    app.run(debug=True)
